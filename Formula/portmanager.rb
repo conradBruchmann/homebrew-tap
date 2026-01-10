e2cfc6e93cd1053580b5b3205f4c13abf2f096ef
Conrad Bruchmann <service@bruchmann.it>
2026-01-10 22:31:29 +0100

    feat: initial release v0.1.0
    Removed service and test definitions from portmanager.rb.

--- Formula/portmanager.rb
+++ Formula/portmanager.rb
@@ -4,10 +4,13 @@ class Portmanager < Formula
   version "0.1.0"
   license "MIT"
 
+
   url "https://github.com/conradBruchmann/PortManager/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "3077ba1a9871f5e2369fdb823afcebebb69824c72230958c710ec8920a449a94"
 
+
   depends_on "rust" => :build
+
 
   def install
     cd "port_manager" do
@@ -16,37 +19,27 @@ class Portmanager < Formula
       bin.install "target/release/client" => "portctl"
     end
 
+
     (var/"portmanager").mkpath
   end
+
 
   def caveats
     <<~EOS
       PortManager has been installed!
 
+
       Start the daemon:
         brew services start portmanager
+
 
       Or run manually:
         portmanager-daemon
 
+
       Dashboard: http://localhost:3030
+
 
       Usage:
         portctl run my-service -- npm start
         portctl list
-        portctl lookup my-service
-    EOS
-  end
-
-  service do
-    run [opt_bin/"portmanager-daemon"]
-    keep_alive true
-    log_path var/"log/portmanager.log"
-    error_log_path var/"log/portmanager.error.log"
-    working_dir var/"portmanager"
-  end
-
-  test do
-    assert_match "Usage:", shell_output("#{bin}/portctl --help", 2)
-  end
-end
